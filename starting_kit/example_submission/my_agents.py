bd31901072675bc30eed1bf875bba0b86b45fa3a
GridL2RPN <grid@chalearn.org>
2019-03-22 17:26:44 +0100

    new data files

--- starting_kit/example_submission/my_agents.py
+++ starting_kit/example_submission/my_agents.py
@@ -56,12 +56,12 @@ class GreedySearch(pypownet.agent.Agent):
     def __init__(self, environment):
         super().__init__(environment)
         self.verbose = False
-
+        self.epsilon = 0.1
         self.ioman = ActIOnManager(destination_path='saved_actions.csv')
         self.ioman2 = ActIOnManager(destination_path='saved_states.csv')
         self.ioman3  = ActIOnManager(destination_path='saved_rewards.csv')
 
-    def act(self, observation):
+    def actGS(self, observation):
         import itertools
 
          # Sanity check: an observation is a structured object defined in the environment file.
@@ -139,7 +139,39 @@ class GreedySearch(pypownet.agent.Agent):
 
         return best_action
 
+    def actRNS(self, observation):
+        # Sanity check: an observation is a structured object defined in the environment file.
+        assert isinstance(observation, pypownet.environment.Observation)
+        action_space = self.environment.action_space
 
+        # Create template of action with no switch activated (do-nothing action)
+        action = action_space.get_do_nothing_action()
+
+        # Select a random substation ID on which to perform node-splitting
+        target_substation_id = np.random.choice(action_space.substations_ids)
+        expected_target_configuration_size = action_space.get_number_elements_of_substation(target_substation_id)
+        # Choses a new switch configuration (binary array)
+        target_configuration = np.random.choice([0, 1], size=(expected_target_configuration_size,))
+
+        action_space.set_switches_configuration_of_substation(action=action,
+                                                              substation_id=target_substation_id,
+                                                              new_configuration=target_configuration)
+
+        # Ensure changes have been done on action
+        current_configuration, _ = action_space.get_switches_configuration_of_substation(action, target_substation_id)
+        assert np.all(current_configuration == target_configuration)
+
+        # Dump best action into stored actions file
+        #self.ioman.dump(action)
+
+        return action
+
+    def act(self, observation):
+        x = random.random()
+        if x<= self.epsilon:
+            return self.actRNS(observation)
+        else:
+            return self.actGS(observation)
 
 class QLearningAgent(pypownet.agent.Agent):
 
@@ -148,13 +180,13 @@ class QLearningAgent(pypownet.agent.Agent):
         self.verbose = True
         self.action_set = self.load_actions(action_file)
         self.state_set = self.load_states(state_file)
+        self.policy = self.compute_polic
 
     def load_actions(self,f):
         pass
 
     def load_states(self, f):
         pass
-
 
     def act(self, observation):
         pass
