08f0941aefef6bca69c07d28d9d1bf7a3ab06c35
GridL2RPN <grid@chalearn.org>
2019-04-15 11:46:02 +0200

    nouvelles donnees

--- starting_kit/script_generation_donnees.sh
+++ starting_kit/script_generation_donnees.sh
@@ -1,10 +1,11 @@
 
-python main.py -a GreedySearch -s 0 -n 100
-python main.py -a GreedySearch -s 1 -n 100
-python main.py -a GreedySearch -s 2 -n 100
-python main.py -a GreedySearch -s 3 -n 100
-python main.py -a GreedySearch -s 4 -n 100
-python main.py -a GreedySearch -s 5 -n 100
-python main.py -a GreedySearch -s 6 -n 100
-python main.py -a GreedySearch -s 7 -n 100
-python main.py -a GreedySearch -s 8 -n 100
+python main.py -a GreedySearch -s 0 -n 2500
+python main.py -a GreedySearch -s 1 -n 2500
+python main.py -a GreedySearch -s 2 -n 2500
+python main.py -a GreedySearch -s 3 -n 2500
+python main.py -a GreedySearch -s 4 -n 2500
+python main.py -a GreedySearch -s 5 -n 2500
+python main.py -a GreedySearch -s 6 -n 2500
+python main.py -a GreedySearch -s 7 -n 2500
+python main.py -a GreedySearch -s 8 -n 2500
+
